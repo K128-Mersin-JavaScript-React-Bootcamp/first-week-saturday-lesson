e25b2b3caab9265976e09f91351e71d4983cb079
Zafer AYAN <zafer.ayan@fibabanka.com.tr>
2021-09-18 12:17:25 +0300

    Add launch.json for debugging

---
+++ index.js
@@ -0,0 +1,17 @@
+var a = 2;
+a = "Zafer";
+a = true;
+a = { name: "Zafer" }
+a = [1,2,3,4]
+a.push(5)
+a.push(true)
+console.log(a);
+
+/*
+let b = 3;
+const c = 4;
+*/
+/*
+Java C#
+int number asdas
+*/
